68778d0145f1e934cfb64e2b02d32aaa75162983
moacirmoda <moa.moda@gmail.com>
2013-06-13 11:20:02 -0300

    removing features in main app that not been used

--- bireme/main/urls.py
+++ bireme/main/urls.py
@@ -2,21 +2,5 @@ from django.conf.urls.defaults import patterns
 
 urlpatterns = patterns('',
     
-    (r'^users/?$', 'main.views.users'),
-    (r'^users/new/?$', 'main.views.new_user'),
-    (r'^users/edit/(?P<user>\d+)/?$', 'main.views.edit_user'),
-    
-    (r'^networks/?$', 'main.views.networks'),
-    (r'^network/new/?$', 'main.views.new_network'),
-    (r'^network/edit/(?P<network>\d+)/?$', 'main.views.edit_network'),
-
-    (r'^services/?$', 'main.views.services'),
-    (r'^services/new/?$', 'main.views.new_service'),
-    (r'^services/edit/(?P<service>\d+)/?$', 'main.views.edit_service'),
-
-    (r'^roles/?$', 'main.views.roles'),
-    (r'^roles/new/?$', 'main.views.new_role'),
-    (r'^roles/edit/(?P<role>\d+)/?$', 'main.views.edit_role'),
-    
     (r'^$', 'main.views.dashboard'),
 )
