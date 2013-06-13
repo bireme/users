a36309abd21e743023b8928d6177e74adaa01219
moacirmoda <moa.moda@gmail.com>
2013-06-13 11:46:03 -0300

    implementing the basics metadata, realted to BVS Passport

--- bireme/main/models.py
+++ bireme/main/models.py
@@ -4,6 +4,7 @@ from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from datetime import datetime
 from django.db import models
+from utils.models import *
 
 class Generic(models.Model):
 
@@ -22,16 +23,33 @@ class Generic(models.Model):
 
 class Profile(models.Model):
 
+    DEGREE_CHOICES = (
+        ('basic education', _('Basic Education')),
+        ('high school', _('High School')),
+        ('technical studies', _('Technical Studies')),
+        ('graduate study', _('Graduate Study')),
+        ('specialization', _('Specialization')),
+        ("professional master's degree", _("Professional Master's Degree")),
+        ("master's degree", _("Master's Degree")),
+        ("doctorate", _('Doctorate')),
+        ("mba", _('MBA')),
+        ("post doctorate", _('Post Doctorate')),
+        ("phd", _('PHD')),
+    )
+
+    GENDER_CHOICES = (
+        ('m', _('Male')),
+        ('f', _('Female')),
+    )
+
     class Meta:
         verbose_name = _("profile")
         verbose_name_plural = _("profiles")
 
-    USER_TYPE_CHOICES = (
-        ('basic', _('Basic')),
-        ('advanced', _('Advanced')),
-    )
-
     user = models.OneToOneField(User, verbose_name="user") # allow extension of default django User
+    degree = models.CharField(_("degree"), max_length=255, blank=True, null=True, choices=DEGREE_CHOICES)
+    country = models.ForeignKey(Country, verbose_name=_("country"), default=30)
+    gender = models.CharField(_("gender"), max_length=1, choices=GENDER_CHOICES)
 
 # creates automatically and profile
 def create_profile(sender, instance, created, **kwargs):
